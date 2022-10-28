46ea1c3beecf0cfd83df9b2aa28b51872496c5e1
Steven Christe <ehsteve@users.noreply.github.com>
2022-10-28 10:48:47 -0400

    Logging and namespace fix
    * fix for logging
    
    * namespace fix
    
    * bug and black fix

--- hermes_spani/calibration/calibration.py
+++ hermes_spani/calibration/calibration.py
@@ -2,7 +2,7 @@
 A module for all things calibration.
 """
 import random
-from hermes_core import log
+from hermes_spani import log
 
 __all__ = ["calibrate_file", "get_calibration_file", "read_calibration_file"]
 
@@ -27,12 +27,6 @@ def calibrate_file(data_filename, output_level=2):
     --------
     """
 
-    calib_file = get_calibration_file(data_filename)
-    if calib_file is None:
-        raise ValueError("Calibration file for {} not found.".format(data_filename))
-    else:
-        calib_data = read_calibration_file(calib_file)
-
     # example log messages
     log.info(
         "Despiking removing {num_spikes} spikes".format(
@@ -44,6 +38,12 @@ def calibrate_file(data_filename, output_level=2):
             num_spikes=random.randint(1, 5)
         )
     )
+
+    calib_file = get_calibration_file(data_filename)
+    if calib_file is None:
+        raise ValueError("Calibration file for {} not found.".format(data_filename))
+    else:
+        calib_data = read_calibration_file(calib_file)
 
     return None
 
