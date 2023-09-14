8c1a474d7c4107e7b1f9e468a07a343e9fd0b2d0
Damian Barrous Dume <damianbarrous@gmail.com>
2023-09-14 14:55:07 +0100

    Update Doc Build, update .gitignore and Remove Cruft (#21)
    * Doc build updates,update to gitignore and remove cruft
    
    * Fix testing to support hermes-core time update
    
    * Fix outdated tests and gitflow

--- hermes_spani/tests/test_calibration.py
+++ hermes_spani/tests/test_calibration.py
@@ -6,8 +6,8 @@ import hermes_spani.calibration as calib
 from hermes_core.util.util import create_science_filename, parse_science_filename
 
 level0_filename = "hermes_SPANI_l0_2022339-000000_v0.bin"
-level1_filename = "hermes_spn_l1_20221205_000000_v1.0.0.cdf"
-ql_filename = "hermes_spn_ql_20221205_000000_v1.0.0.cdf"
+level1_filename = "hermes_spn_l1_20221205T000000_v1.0.0.cdf"
+ql_filename = "hermes_spn_ql_20221205T000000_v1.0.0.cdf"
 
 
 @pytest.fixture(scope="session")
